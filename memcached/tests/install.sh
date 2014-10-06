d961fe5f21b8ac474e2366d8d2b12704f8511c5f
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-06 18:51:21 +0300

    update install.sh to use prepackaged libmemcached

--- memcached/tests/install.sh
+++ memcached/tests/install.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-if [ `memaslap -h | wc -l` -gt 1 ]  
-    then  
-       echo "already installed"
-       exit 0
-fi
-
-FILE=https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
-
-wget $FILE
-tar xvf  $FILE
-
-mv libmemcached* libmemcached
-cd libmemcached
-./configure
-make
-
+sudo yum install -y libmemcached
