f517dc70acc66d5aae7cc800883049728edd8466
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-11 13:00:53 +0300

    update cassandra install

--- cassandra/tests/install.sh
+++ cassandra/tests/install.sh
@@ -1,6 +1,4 @@
 wget https://github.com/downloads/brianfrankcooper/YCSB/ycsb-0.1.4.tar.gz
 tar xfvz ycsb-0.1.4.tar.gz
-cd ycsb-0.1.4/cassandra
-mvn package install
 
 chmod +x ../upstream/current/tools/bin/*
