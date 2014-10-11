08d1993b25128e9da710abb2b8e4085769ef48f3
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-11 13:04:44 +0300

    moving params into configuration

--- cassandra/tests/test_1_cassandrastress_read/tester/step_3_run.template.sh
+++ cassandra/tests/test_1_cassandrastress_read/tester/step_3_run.template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-../../../upstream/current/tools/bin/cassandra-stress read n=100000 -node $sut_ip -rate threads=50
+../../../upstream/current/tools/bin/cassandra-stress read n=$tester-cassandra-stress-read_run_requests -node $sut_ip -rate threads=50
