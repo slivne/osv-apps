330d8260cf603208210937eff8f8714a212e38fd
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-11 13:16:41 +0300

    update reddis

--- redis-memonly/tests/test_1_redis_benchmark/tester/step_1_run.template.sh
+++ redis-memonly/tests/test_1_redis_benchmark/tester/step_1_run.template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-../../upstream/redis/src/redis-benchmark -h $sut_ip -n $sut_redis_requests -c $sut_redis_clients
+../../upstream/redis/src/redis-benchmark -h $sut_ip -n $tester_redis_requests -c $tester_redis_clients
