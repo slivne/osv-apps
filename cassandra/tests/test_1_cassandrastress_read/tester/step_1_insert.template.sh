407550ead05186ec96a2b53b15c8a861a427e0f6
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-11 12:12:54 +0300

    update cassandra

--- cassandra/tests/test_1_cassandrastress_read/tester/step_1_insert.template.sh
+++ cassandra/tests/test_1_cassandrastress_read/tester/step_1_insert.template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-../../upstream/current/tools/bin/cassandra-stress write n=100000 -node $sut_ip -rate threads=50
+../../../upstream/current/tools/bin/cassandra-stress write n=100000 -node $sut_ip -rate threads=50
