13f7bcec3dc622b0d8d84c36126e1ef2458abb31
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-11 15:17:56 +0300

    update with env

--- cassandra/tests/install.sh
+++ cassandra/tests/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -e
 cd apps/cassandra
 
 wget https://github.com/downloads/brianfrankcooper/YCSB/ycsb-0.1.4.tar.gz
@@ -5,4 +7,7 @@ tar xfvz ycsb-0.1.4.tar.gz
 
 make upstream/apache-cassandra-2.1.0
 
-chmod +x upstream/current/tools/bin/*
+chmod +x upstream/apache-cassandra-2.1.0/tools/bin/*
+
+echo export CASSANDRA_STRESS="`pwd`/upstream/apache-cassandra-2.1.0/tools/bin/cassandra-stress" > tests/setenv.sh
+echo export YCSB_ROOT="`pwd`/ycsb-0.1.4" >> tests/setenv.sh
--- cassandra/tests/test_1_cassandrastress_read/tester/step_3_run.template.sh
+++ cassandra/tests/test_1_cassandrastress_read/tester/step_3_run.template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-../../../upstream/current/tools/bin/cassandra-stress read n=$tester_cassandra-stress-read_run_requests -node $sut_ip -rate threads=50
+$CASSANDRA_STRESS read n=$tester_cassandra-stress-read_run_requests -node $sut_ip -rate threads=50
