863850f48132d88dfbc8efeb06e99648eb96c0bd
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-12 16:39:57 +0300

    update code

--- cassandra/tests/test_2_durability_cassandrastress/tester/step_2_run.template.sh
+++ cassandra/tests/test_2_durability_cassandrastress/tester/step_2_run.template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 STARTTIME=$(date +%s)
-let ENDTIME=$STARTTIME + 60*60*$$tester.duration_in_hours
+let ENDTIME=$STARTTIME+60*60*$$tester.duration_in_hours
 
 while [ $(date %s) -lt $ENDTIME ]; do
     $CASSANDRA_STRESS read n=500000 -node $$sut.ip -rate threads=50
