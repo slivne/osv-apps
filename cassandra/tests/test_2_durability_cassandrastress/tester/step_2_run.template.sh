999cd500ab91155ce58dbcc4246b31ded8a8b835
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-12 18:06:32 +0300

    fix bug

--- cassandra/tests/test_2_durability_cassandrastress/tester/step_2_run.template.sh
+++ cassandra/tests/test_2_durability_cassandrastress/tester/step_2_run.template.sh
@@ -3,6 +3,6 @@ set -e
 STARTTIME=$(date +%s)
 let ENDTIME=$STARTTIME+60*60*$$tester.duration_in_hours
 
-while [ $(date %s) -lt $ENDTIME ]; do
+while [ $(date +%s) -lt $ENDTIME ]; do
     $CASSANDRA_STRESS read n=500000 -node $$sut.ip -rate threads=50
 done
