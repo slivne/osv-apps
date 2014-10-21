f99aa7512ebd8f7ac8aad9d2d9b26e58c667f01a
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-21 09:13:23 +0300

    update with review comments

--- redis-memonly/tests/test_1_redis_benchmark/tester/step_1_run.template.sh
+++ redis-memonly/tests/test_1_redis_benchmark/tester/step_1_run.template.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 set -e
-$REDIS_BENCHMARK -h $$sut.ip -n $$tester.redis.requests -c $$tester.redis.clients
+
+for clients in $$tester.ycsb.run.clients ; do
+    for pipelines in $$tester.ycsb.run.pipelines ; do
+        echo "start test
+        echo "clients : $clients"
+        echo "pipeline : $pipelines"
+        $REDIS_BENCHMARK -h $$sut.ip -n $$tester.redis.requests -c $clients -P $pipelines
+        echo "end test"
+    done
+done
