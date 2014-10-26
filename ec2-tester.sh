c0e8cb149cc774be354a979c17622ba4c4093714
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-26 10:33:08 +0200

    echo tester execution for debug

--- ec2-tester.sh
+++ ec2-tester.sh
@@ -174,6 +174,7 @@ ping -c 4 $TEST_INSTANCE_IP
 echo "=== Run tester ==="
 # TODO FIX LOCAL IP
 selector="ec2_$INSTANCE_TYPE"
+echo "$SCRIPTS_ROOT/tester.py run --config_param sut.ip:$TEST_INSTANCE_IP --config_param tester.ip:127.0.0.1 --config_selection $selector $TESTS"
 $SCRIPTS_ROOT/tester.py run --config_param sut.ip:$TEST_INSTANCE_IP --config_param tester.ip:127.0.0.1 --config_selection $selector $TESTS  || handle_test_error
 
 ec2-get-console-output $TEST_INSTANCE_ID
