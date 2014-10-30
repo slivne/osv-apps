2bb297b5cb991197368681cbc1f981c531b9697e
Shlomi Livne <shlomi@cloudius-systems.com>
2014-10-30 23:55:56 +0200

    remove

--- cassandra/tests/test_sanity/tester/step_1_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-$CASSANDRA_STRESS write n=10 -node 192.168.122.89 -rate threads=10
-$CASSANDRA_STRESS read n=10 -node 192.168.122.89 -rate threads=10
-
-$GENERIC_ROOT/rest/base.sh 192.168.122.89
