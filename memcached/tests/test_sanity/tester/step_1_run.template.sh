4caf0bdb17c18d4a1dfa56c1a389de0474d7af98
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-02 12:22:49 +0200

    limit to udp

--- memcached/tests/test_sanity/tester/step_1_run.template.sh
+++ memcached/tests/test_sanity/tester/step_1_run.template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-memslap -s $$sut.ip:11211  --concurrency=10 --execute-number=5
+memslap -s $$sut.ip:11211 --udp --concurrency=10 --execute-number=5
 
 $GENERIC_ROOT/rest/base.sh $$sut.ip
