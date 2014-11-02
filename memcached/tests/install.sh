f165aa8d7da389bf2054fd6ada5f4dd9d247888d
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-02 10:14:24 +0200

    disable memaslap

--- memcached/tests/install.sh
+++ memcached/tests/install.sh
@@ -9,7 +9,8 @@ if [ ! -e /usr/bin/memaslap ]; then
    wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
    tar xvf libmemcached-1.0.18.tar.gz
    cd libmemcached-1.0.18
-   ./configure --enable-memaslap
+   ./configure
+#   ./configure --enable-memaslap
    sudo make install 
    cd ..
 fi
