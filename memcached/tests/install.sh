f916420a88abcf1b09e842e770cb4ad54cbf55f1
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-02 11:51:11 +0200

    update

--- memcached/tests/install.sh
+++ memcached/tests/install.sh
@@ -5,16 +5,16 @@ tests_dir=${0%/*}
 
 cd $tests_dir
 
-if [ ! -e /usr/bin/memaslap ]; then 
+if [ ! -e /usr/local/bin/memaslap ]; then 
    wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
    tar xvf libmemcached-1.0.18.tar.gz
    cd libmemcached-1.0.18
    ./configure
 #   ./configure --enable-memaslap
    sudo make install 
-   sudo ldconfig /usr/local/lib
    cd ..
 fi
+sudo ldconfig /usr/local/lib
 
 ../../tester/generic/install.sh
 cat ../../tester/generic/setenv.sh > setenv.sh
