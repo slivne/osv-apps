e228742ce1fe4ea01ce409337fd9e0e9e55be2cd
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-02 09:03:44 +0200

    update install

--- memcached/tests/install.sh
+++ memcached/tests/install.sh
@@ -5,7 +5,14 @@ tests_dir=${0%/*}
 
 cd $tests_dir
 
-sudo yum install -y libmemcached
+if [ ! -e /usr/bin/memaslap ]; then 
+   wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
+   tar xvf libmemcached-1.0.18.tar.gz
+   cd libmemcached-1.0.18
+   ./configure
+   sudo make install 
+   cd ..
+fi
 
 ../../tester/generic/install.sh
-cat ../../tester/generic/setenv.sh >! tests/setenv.sh
+cat ../../tester/generic/setenv.sh > setenv.sh
