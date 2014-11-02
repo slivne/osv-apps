d8d43cf4a721dccdac1db4072d8573dcac629911
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-02 13:34:05 +0200

    add ec2-tester

--- ec2-tester.sh
+++ ec2-tester.sh
@@ -207,7 +207,7 @@ if test x"$AMI_ID" = x""; then
    prepare_image_for_test
    create_ami
 fi
-TEST_INSTANCE_NAME=OSv-`get_ami_name_by_id $AMI_ID`-`timestamp`
+TEST_INSTANCE_NAME=OSv-`get_ami_name_by_id $AMI_ID`-ec2-tester-`timestamp`
 
 
 for TEST in "$TESTS";
