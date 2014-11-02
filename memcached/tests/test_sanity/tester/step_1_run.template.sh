d055b3d054e1ad932aacbd12161312ac31e9df2a
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-02 10:16:00 +0200

    move to memslap

--- memcached/tests/test_sanity/tester/step_1_run.template.sh
+++ memcached/tests/test_sanity/tester/step_1_run.template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-memaslap -s $$sut.ip:11211 --threads=10 --concurrency=10 --time=30s
+memslap -s $$sut.ip:11211  --concurrency=10 --execute-number=5
 
 $GENERIC_ROOT/rest/base.sh $$sut.ip
