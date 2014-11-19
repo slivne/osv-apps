fdc3f193f6632d1ed09431ad89d9b13935f7e8c0
Amnon Heiman <amnon@cloudius-systems.com>
2014-11-19 16:53:19 +0200

    add batch file for user info and multiple retry

---
+++ memcached/tests/linux_setup.sh
@@ -0,0 +1,25 @@
+"#!/bin/bash
+echo start >> /tmp/boot
+yum update -y
+yum install -y wget
+yum install -y gcc
+cd /tmp
+mkdir a
+cd a
+wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
+tar xzvf libevent-2.0.21-stable.tar.gz
+cd libevent-2.0.21-stable
+./configure
+make 
+make install
+ldconfig /usr/local/lib
+cd ..
+wget http://memcached.org/latest
+tar -zxvf latest
+cd memcached-1.4.??
+./configure 
+make
+make test
+make install
+nohup memcached -u root &
+echo end >> /tmp/boot"
