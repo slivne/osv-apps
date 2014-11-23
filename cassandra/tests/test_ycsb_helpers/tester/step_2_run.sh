a34e3a699e208ef5249b5d3cfe2c01a55ba5ae9b
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-23 23:33:25 +0200

    pass 10 param

--- cassandra/tests/test_ycsb_helpers/tester/step_2_run.sh
+++ cassandra/tests/test_ycsb_helpers/tester/step_2_run.sh
@@ -7,7 +7,7 @@ for threads in $3 ; do
       echo "cs>> start"
       echo "cs>> iteration : $iteration"
       echo "cs>> threads : $threads"
-      $YCSB_ROOT/bin/ycsb run cassandra-10 -threads $threads -p operationcount=$5 -p recordcount=$6 -p fieldcount=$7 -p maxexecutiontime=$8 -p requestdistribution=$9 -p maxscanlength=$10 -p hosts=$1 -P $YCSB_ROOT/workloads/$2 -s 
+      $YCSB_ROOT/bin/ycsb run cassandra-10 -threads $threads -p operationcount=$5 -p recordcount=$6 -p fieldcount=$7 -p maxexecutiontime=$8 -p requestdistribution=$9 -p maxscanlength=${10} -p hosts=$1 -P $YCSB_ROOT/workloads/$2 -s 
       let iteration=$iteration+1
       echo "cs>> end"
    done
