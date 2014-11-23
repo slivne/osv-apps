1a592b46b92060c0b6c49bb72ae579c4102cf206
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-23 18:31:57 +0200

    fix param fieldcount

--- cassandra/tests/test_5_ycsb_workload_c/tester/step_2_run.template.sh
+++ cassandra/tests/test_5_ycsb_workload_c/tester/step_2_run.template.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-../../test_ycsb_helpers/tester/step_2_run.sh $$sut.ip workloadc "$$tester.ycsb.run.threads" $$tester.ycsb.run.iterations $$tester.ycsb.run.maxoperationcount $$tester.ycsb.recordcount $$tester.tester.ycsb.fieldcount $$tester.ycsb.run.maxexecutiontimeinseconds zipfian 100
+../../test_ycsb_helpers/tester/step_2_run.sh $$sut.ip workloadc "$$tester.ycsb.run.threads" $$tester.ycsb.run.iterations $$tester.ycsb.run.maxoperationcount $$tester.ycsb.recordcount $$tester.ycsb.fieldcount $$tester.ycsb.run.maxexecutiontimeinseconds zipfian 100
