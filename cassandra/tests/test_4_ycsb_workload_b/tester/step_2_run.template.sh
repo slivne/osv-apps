061328d85a5faddd40f80e81f6c5f43446872b56
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-24 09:59:11 +0200

    update configuration to support ycsb extended support - like workload c

--- cassandra/tests/test_4_ycsb_workload_b/tester/step_2_run.template.sh
+++ cassandra/tests/test_4_ycsb_workload_b/tester/step_2_run.template.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-../../test_ycsb_helpers/tester/step_2_run.sh $$sut.ip workloadb "$$tester.ycsb.run.threads" $$tester.ycsb.run.iterations $$tester.ycsb.run.operationcount $$tester.ycsb.recordcount
+../../test_ycsb_helpers/tester/step_2_run.sh $$sut.ip workloadb "$$tester.ycsb.run.threads" $$tester.ycsb.run.iterations $$tester.ycsb.run.maxoperationcount $$tester.ycsb.recordcount $$tester.ycsb.fieldcount $$tester.ycsb.run.maxexecutiontimeinseconds $$tester.ycsb.requestdistribution 100
--- cassandra/tests/test_7_ycsb_workload_e/tester/step_1_insert.template.sh
+++ cassandra/tests/test_7_ycsb_workload_e/tester/step_1_insert.template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-../../test_ycsb_helpers/tester/step_1_insert.sh  $$sut.ip workloade $$tester.ycsb.load.threads $$tester.ycsb.load.operationcount $$tester.ycsb.recordcount
+../../test_ycsb_helpers/tester/step_1_insert.sh  $$sut.ip workloade $$tester.ycsb.load.threads $$tester.ycsb.recordcount $$tester.ycsb.recordcount $$tester.ycsb.fieldcount $$tester.ycsb.requestdistribution 
