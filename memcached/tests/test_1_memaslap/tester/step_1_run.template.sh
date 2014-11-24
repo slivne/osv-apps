b6afc564a476bee78b4164654a43f65221b8c15f
Amnon Heiman <amnon@cloudius-systems.com>
2014-11-24 13:27:56 +0200

    updated test files with the new functionality

--- memcached/tests/test_1_memaslap/tester/step_1_run.template.sh
+++ memcached/tests/test_1_memaslap/tester/step_1_run.template.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -e
-memaslap -s $$sut.ip:11211 --threads=$$tester.memaslap.threads --concurrency=$$tester.memaslap.concurrency --time=$$tester.memaslap.duration
+for i in `seq 1 $$tester.memaslap.retry`; do
+	memaslap -s $$sut.ip:11211 --threads=$$tester.memaslap.threads --concurrency=$$tester.memaslap.concurrency --time=$$tester.memaslap.duration
+	echo "$$tester.memaslap.test_delimiter"
+done
+#memaslap -s $$sut.ip:11211 --threads=$$tester.memaslap.threads --concurrency=$$tester.memaslap.concurrency --time=$$tester.memaslap.duration
