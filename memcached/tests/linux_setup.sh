13617f40a6c2f92f23d3bb580f588537f2c6592a
Amnon Heiman <amnon@cloudius-systems.com>
2014-11-25 10:25:14 +0200

    adding timestamp and info in boot time

--- memcached/tests/linux_setup.sh
+++ memcached/tests/linux_setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo start >> /tmp/boot
+echo "installing wget gcc" `uptime` >> /tmp/boot 
 yum update -y
 yum install -y wget
 yum install -y gcc
 cd /tmp
 mkdir a
 cd a
+echo "Downloading " `uptime` >> /tmp/boot 
 wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
 tar xzvf libevent-2.0.21-stable.tar.gz
 cd libevent-2.0.21-stable
@@ -21,5 +23,7 @@ cd memcached-1.4.??
 make
 make test
 make install
+echo "Starting memcached " `uptime` >> /tmp/boot
 nohup memcached -u root -t 1 -m 4096 &
 echo end >> /tmp/boot
+echo `uptime` >> /tmp/boot
