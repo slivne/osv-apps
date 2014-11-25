972dd76de5a7fee0b38ae4cbfe210c5120940fe6
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-25 10:14:38 +0200

    update yo use out directory

--- cassandra/tests/test_ycsb_helpers/tester/step_3_result.sh
+++ cassandra/tests/test_ycsb_helpers/tester/step_3_result.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-../../test_ycsb_helpers/tester/ycsbparse.py step_2_run.sh.stdout_stderr > result.txt
+../../test_ycsb_helpers/tester/ycsbparse.py out/step_2_run.sh.stdout_stderr > out/result.txt
