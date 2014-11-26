8dd65b3c79c1b3b2b2f5e4773ae8c11be5542a1d
Shlomi Livne <shlomi@cloudius-systems.com>
2014-11-26 09:00:49 +0200

    adding warmuo

--- cassandra/tests/test_3_ycsb_workload_a/tester/step_2_run.template.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-../../test_ycsb_helpers/tester/step_2_run.sh $$sut.ip workloada "$$tester.ycsb.run.threads" $$tester.ycsb.run.iterations $$tester.ycsb.run.maxoperationcount $$tester.ycsb.recordcount $$tester.ycsb.fieldcount $$tester.ycsb.run.maxexecutiontimeinseconds $$tester.ycsb.requestdistribution 100
---
+++ cassandra/tests/test_3_ycsb_workload_a/tester/step_3_run.template.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+../../test_ycsb_helpers/tester/step_3_run.sh $$sut.ip workloada "$$tester.ycsb.run.threads" $$tester.ycsb.run.iterations $$tester.ycsb.run.maxoperationcount $$tester.ycsb.recordcount $$tester.ycsb.fieldcount $$tester.ycsb.run.maxexecutiontimeinseconds $$tester.ycsb.requestdistribution 100
