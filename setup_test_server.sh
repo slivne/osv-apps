1d6989617d535adc3e8e8a461656dca9f3cd8f41
Amnon Heiman <amnon@cloudius-systems.com>
2014-11-26 18:35:12 +0200

    install with -y

--- setup_test_server.sh
+++ setup_test_server.sh
@@ -13,7 +13,7 @@ sudo echo "gpgcheck=1" >> /etc/yum.repos.d
 sudo echo "gpgkey=http://s3tools.org/repo/RHEL_6/repodata/repomd.xml.key" >> /etc/yum.repos.d
 sudo echo "enabled=1" >> /etc/yum.repos.d
 
-sudo yum install s3cmd
+sudo yum install -y s3cmd
 cat s3cfg |sed "s/access_key = XXX/access_key = $AWS_ACCESS_KEY_ID/" | sed "s/secret_key = XXX/secret_key = $AWS_SECRET_ACCESS_KEY/" >  ~/.s3cfg
 cd ~/osv/apps/$TEST_APP/tests
 ./install.sh
