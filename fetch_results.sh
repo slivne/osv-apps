1f0dc9ea2480cfb2a748737ccb0fb4364ed60e9a
Amnon Heiman <amnon@cloudius-systems.com>
2014-12-01 14:38:10 +0200

    add image name parameter to fetch result

--- fetch_results.sh
+++ fetch_results.sh
@@ -3,8 +3,10 @@
 
 PARAM_HELP_LONG="--help"
 PARAM_ALL_FILES="--all"
+PARAM_IMAGE="--image"
 
 ALL_FILES=0
+IMAGE=""
 ROOT_SRC=$(pwd)
 
 if test x"$TEST_BUCKET" = x""; then
@@ -24,6 +26,7 @@ If it's a bucket name, the latest test will be retrieve
 This script receives following command line arguments:   
     $PARAM_HELP_LONG - print this help screen and exit
     $PARAM_ALL_FILES - retrieve all files in the bucket
+    $PARAM_IMAGE - return results of a specific image type
     <bucket_name> - bucket name to download, can be set with BUCKET_DIR environment
 HLPEND
 }
@@ -38,6 +41,10 @@ do
     "$PARAM_ALL_FILES")
       ALL_FILES=1
       shift
+      ;;
+    "$PARAM_IMAGE")
+      IMAGE="$2"
+      shift 2
       ;;
       *)
       TEST_BUCKET=$1
@@ -56,17 +63,23 @@ fi
 handle_file() {
     NAME=$1
      if [[ $NAME  =~ ([^/]+)\.zip$ ]]; then
-        OUT_DIR="${BASH_REMATCH[1]}"
+        FILE_NAME="${BASH_REMATCH[1]}"
+        if [ $IMAGE == "" ]; then
+          OUT_DIR="$FILE_NAME"
+        else
+          OUT_DIR="$IMAGE/$FILE_NAME"
+        fi
         if [ -d "$OUT_DIR" ]; then
           echo "$OUT_DIR exists, skipping"
         else
-	        mkdir "$OUT_DIR"
+	        mkdir -p "$OUT_DIR"
 	        cd "$OUT_DIR" || exit -1
 	        s3cmd get "$NAME"
 	        unzip ./*.zip
-	        rm "$OUT_DIR".zip
-	        for f in *.stdout_stderr; do
-	           $ROOT_SRC/memaslap2json.py --delimiter '>>>>>>> end <<<<<<<' $f | $ROOT_SRC/statjson.py > "$f.json"
+	        rm "$FILE_NAME".zip
+	        for f in *.json; do
+             $ROOT_SRC/statjson.py $f > "$IMAGE.json"
+#	           	           $ROOT_SRC/memaslap2json.py --delimiter '>>>>>>> end <<<<<<<' $f | $ROOT_SRC/statjson.py > "$f.json"
 	        done
 	        cd -
         fi
@@ -77,10 +90,10 @@ if [[ "$TEST_BUCKET" == *.zip ]]; then
   handle_file "$TEST_BUCKET"
 else
   if [ "$ALL_FILES" -eq 0 ]; then
-    FILE=$(s3cmd ls "$TEST_BUCKET"/* | awk '{print $4}' |sort | tail -1)
+    FILE=$(s3cmd ls "$TEST_BUCKET/$IMAGE"/* | awk '{print $4}' |sort | tail -1)
     handle_file "$FILE"
   else
-    FILES=$(s3cmd ls "$TEST_BUCKET"/* | awk '{print $4}')
+    FILES=$(s3cmd ls "$TEST_BUCKET/$IMAGE"/* | awk '{print $4}')
     for FILE in ${FILES} ;
     do
        handle_file "$FILE"
